4594b5133131de6f39683a3038ff12b3fd0fc2af
enes-yvz <97enesyavuz@gmail.com>
2022-08-17 20:39:15 +0300

    Small changes

--- src/main/java/yte/intern/springapplication/authentication/configuration/PasswordEncoderConfiguration.java
+++ src/main/java/yte/intern/springapplication/authentication/configuration/PasswordEncoderConfiguration.java
@@ -13,7 +13,5 @@ public class PasswordEncoderConfiguration {
 
         return new BCryptPasswordEncoder();
 
-        //return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-
     }
 }
